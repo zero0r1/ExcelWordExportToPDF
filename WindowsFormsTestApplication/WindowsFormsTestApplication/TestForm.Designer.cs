f5364a3071e0165c004d8b07c622ab1a6ab4bb24
zero0r1 <leahdizonth@gmail.com>
2017-08-24 10:59:52 +0800

    Commit

---
+++ WindowsFormsTestApplication/WindowsFormsTestApplication/TestForm.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace WindowsFormsTestApplication
+{
+    partial class TestForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TestForm));
+            this.label1 = new System.Windows.Forms.Label();
+            this.labSourceExcel = new System.Windows.Forms.Label();
+            this.btnExcel = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.labSourceWord = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnWord = new System.Windows.Forms.Button();
+            this.btnConvertPDF = new System.Windows.Forms.Button();
+            this.btn = new System.Windows.Forms.Button();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "File Address:";
+            // 
+            // labSourceExcel
+            // 
+            this.labSourceExcel.AutoSize = true;
+            this.labSourceExcel.Location = new System.Drawing.Point(78, 13);
+            this.labSourceExcel.Name = "labSourceExcel";
+            this.labSourceExcel.Size = new System.Drawing.Size(0, 12);
+            this.labSourceExcel.TabIndex = 1;
+            // 
+            // btnExcel
+            // 
+            this.btnExcel.Location = new System.Drawing.Point(555, 8);
+            this.btnExcel.Name = "btnExcel";
+            this.btnExcel.Size = new System.Drawing.Size(98, 23);
+            this.btnExcel.TabIndex = 2;
+            this.btnExcel.Text = "Choose Excel";
+            this.btnExcel.UseVisualStyleBackColor = true;
+            this.btnExcel.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // labSourceWord
+            // 
+            this.labSourceWord.AutoSize = true;
+            this.labSourceWord.Location = new System.Drawing.Point(78, 39);
+            this.labSourceWord.Name = "labSourceWord";
+            this.labSourceWord.Size = new System.Drawing.Size(0, 12);
+            this.labSourceWord.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 39);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 12);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "File Address:";
+            // 
+            // btnWord
+            // 
+            this.btnWord.Location = new System.Drawing.Point(455, 10);
+            this.btnWord.Name = "btnWord";
+            this.btnWord.Size = new System.Drawing.Size(98, 23);
+            this.btnWord.TabIndex = 5;
+            this.btnWord.Text = "Choose Word";
+            this.btnWord.UseVisualStyleBackColor = true;
+            this.btnWord.Click += new System.EventHandler(this.btnWord_Click);
+            // 
+            // btnConvertPDF
+            // 
+            this.btnConvertPDF.Location = new System.Drawing.Point(555, 39);
+            this.btnConvertPDF.Name = "btnConvertPDF";
+            this.btnConvertPDF.Size = new System.Drawing.Size(98, 23);
+            this.btnConvertPDF.TabIndex = 6;
+            this.btnConvertPDF.Text = "Excel To PDF";
+            this.btnConvertPDF.UseVisualStyleBackColor = true;
+            this.btnConvertPDF.Click += new System.EventHandler(this.btnConvertPDF_Click);
+            // 
+            // btn
+            // 
+            this.btn.Location = new System.Drawing.Point(455, 39);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(98, 23);
+            this.btn.TabIndex = 7;
+            this.btn.Text = "Word To PDF";
+            this.btn.UseVisualStyleBackColor = true;
+            this.btn.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(15, 79);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(432, 180);
+            this.checkedListBox1.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(555, 68);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "PDF To Each";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_2);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(555, 97);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(98, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "PDF To Marge";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // TestForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(661, 268);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.btn);
+            this.Controls.Add(this.btnConvertPDF);
+            this.Controls.Add(this.btnWord);
+            this.Controls.Add(this.labSourceWord);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnExcel);
+            this.Controls.Add(this.labSourceExcel);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "TestForm";
+            this.Text = "TestForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labSourceExcel;
+        private System.Windows.Forms.Button btnExcel;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label labSourceWord;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnWord;
+        private System.Windows.Forms.Button btnConvertPDF;
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ WindowsFormsTestApplication/WindowsFormsTestApplication/TestForm.cs
@@ -0,0 +1,336 @@
+﻿using Microsoft.Office.Interop.Word;
+using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using Word = Microsoft.Office.Interop.Word;
+
+
+namespace WindowsFormsTestApplication
+{
+    public partial class TestForm : Form
+    {
+        public TestForm()
+        {
+            InitializeComponent();
+        }
+
+        List<string> lstAddress = new List<string>();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Excel Files|*.xls;*.xlsx;*.xlsm";
+            openFileDialog1.FileName = string.Empty;
+            Excel.Sheets oSheets = null;
+            Excel.Workbook excelWorkBook = null;
+            Excel.ApplicationClass application = null;
+            object paramMissing = Type.Missing;
+
+            try
+            {
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    this.labSourceExcel.Text = openFileDialog1.FileName;
+                }
+
+                application = new Excel.ApplicationClass();
+                excelWorkBook = application.Workbooks.Open(openFileDialog1.FileName);
+                oSheets = excelWorkBook.Worksheets;
+
+                this.checkedListBox1.Items.Clear();
+                foreach (Excel.Worksheet oSheet in oSheets)
+                {
+                    this.checkedListBox1.Items.Add(oSheet.Name);
+                }
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+                if (excelWorkBook != null)
+                {
+                    excelWorkBook.Close(false, paramMissing, paramMissing);
+                    excelWorkBook = null;
+                }
+                if (application != null)
+                {
+                    application.Quit();
+                    application = null;
+                }
+
+                //ms solution is like this
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+            }
+
+        }
+
+        private void btnWord_Click(object sender, EventArgs e)
+        {
+            //"Excel Files|*.xls;*.xlsx;*.xlsm|Word Files|*.docx"
+            openFileDialog1.Filter = "Word Files|*.docx";
+            openFileDialog1.FileName = string.Empty;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                this.labSourceWord.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void button1_Click_2(object sender, EventArgs e)
+        {
+            XLSConvertToPDFEach(this.labSourceExcel.Text);
+        }
+
+        private void btnConvertPDF_Click(object sender, EventArgs e)
+        {
+            XLSConvertToPDF(this.labSourceExcel.Text);
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+            DOCXConvertToPDF(this.labSourceWord.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            XLSConvertToPDFMarge(this.labSourceExcel.Text);
+
+        }
+
+        bool XLSConvertToPDF(string sourceFile)
+        {
+            string sourcePath = sourceFile;
+            string targetPath = GetFileName(sourceFile);
+            bool result = false;
+            Excel.XlFixedFormatType targetType = Microsoft.Office.Interop.Excel.XlFixedFormatType.xlTypePDF;// Excel.XlFixedFormatType.xlTypePDF;
+            object paramMissing = Type.Missing;
+            Excel.ApplicationClass application = null;
+            Excel.Workbook excelWorkBook = null;
+            try
+            {
+                application = new Excel.ApplicationClass();
+                object target = targetPath;
+                object type = targetType;
+                excelWorkBook = application.Workbooks.Open(sourcePath);
+
+                Excel.Worksheet xlSheet = (Excel.Worksheet)excelWorkBook.Worksheets[1];
+
+                if (excelWorkBook != null)
+                {
+                    excelWorkBook.ExportAsFixedFormat(targetType, target, Excel.XlFixedFormatQuality.xlQualityStandard, true, true, paramMissing, paramMissing, false, paramMissing);
+                    result = true;
+                }
+            }
+            catch
+            {
+                result = false;
+            }
+            finally
+            {
+                if (excelWorkBook != null)
+                {
+                    excelWorkBook.Close(false, paramMissing, paramMissing);
+                    excelWorkBook = null;
+                }
+                if (application != null)
+                {
+                    application.Quit();
+                    application = null;
+                }
+
+                //ms solution is like this
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+            }
+            return result;
+        }
+
+        bool XLSConvertToPDFEach(string sourceFile)
+        {
+            string sourcePath = sourceFile;
+            string targetPath = GetFileName(sourceFile);
+            bool result = false;
+            Excel.XlFixedFormatType targetType = Microsoft.Office.Interop.Excel.XlFixedFormatType.xlTypePDF;// Excel.XlFixedFormatType.xlTypePDF;
+            object paramMissing = Type.Missing;
+            Excel.ApplicationClass application = null;
+            Excel.Workbook excelWorkBook = null;
+            Excel.Worksheet excelWorksheet = null;
+            try
+            {
+                application = new Excel.ApplicationClass();
+                object target = targetPath;
+                object type = targetType;
+                excelWorkBook = application.Workbooks.Open(sourcePath);
+                List<string> lstSheets = new List<string>();
+
+                foreach (string item in this.checkedListBox1.CheckedItems)
+                {
+                    excelWorksheet = (Excel.Worksheet)excelWorkBook.Worksheets[item];
+                    if (excelWorksheet != null)
+                    {
+                        excelWorksheet.ExportAsFixedFormat(targetType, target + "_" + excelWorksheet.Name, Excel.XlFixedFormatQuality.xlQualityStandard, true, paramMissing, paramMissing, paramMissing, false, paramMissing);
+                        result = true;
+                    }
+                }
+            }
+            catch
+            {
+                result = false;
+            }
+            finally
+            {
+                if (excelWorkBook != null)
+                {
+                    excelWorkBook.Close(false, paramMissing, paramMissing);
+                    excelWorkBook = null;
+                }
+                if (application != null)
+                {
+                    application.Quit();
+                    application = null;
+                }
+
+                //ms solution is like this
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+            }
+            return result;
+
+        }
+
+        bool XLSConvertToPDFMarge(string sourceFile)
+        {
+            string sourcePath = sourceFile;
+            string targetPath = GetFileName(sourceFile);
+            bool result = false;
+            Excel.XlFixedFormatType targetType = Microsoft.Office.Interop.Excel.XlFixedFormatType.xlTypePDF;// Excel.XlFixedFormatType.xlTypePDF;
+            object paramMissing = Type.Missing;
+            Excel.ApplicationClass application = null;
+            Excel.Workbook excelWorkBook = null;
+            Excel.Worksheet excelWorksheet = null;
+            Excel.Worksheet sheetDelete = null;
+            try
+            {
+                application = new Excel.ApplicationClass();
+                object target = targetPath;
+                object type = targetType;
+                excelWorkBook = application.Workbooks.Open(sourcePath);
+                List<string> lstSheets = new List<string>();
+
+                for (int i = 0; i < this.checkedListBox1.Items.Count; i++)
+                {
+                    if (this.checkedListBox1.GetItemCheckState(i) == CheckState.Unchecked)
+                    {
+                        string deleteSheetName = this.checkedListBox1.Items[i].ToString();
+                        sheetDelete = (Excel.Worksheet)excelWorkBook.Worksheets[deleteSheetName];
+
+                        if (sheetDelete != null)
+                        {
+                            sheetDelete.Delete();
+                        }
+
+                        sheetDelete = null;
+                    }
+                }
+
+                excelWorkBook.ExportAsFixedFormat(targetType, target, Excel.XlFixedFormatQuality.xlQualityStandard, true, true, paramMissing, paramMissing, false, paramMissing);
+                result = true;
+            }
+            catch
+            {
+                result = false;
+            }
+            finally
+            {
+                if (excelWorkBook != null)
+                {
+                    excelWorkBook.Close(false, paramMissing, paramMissing);
+                    excelWorkBook = null;
+                }
+                if (application != null)
+                {
+                    application.Quit();
+                    application = null;
+                }
+
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+            }
+            return result;
+        }
+
+        bool DOCXConvertToPDF(string sourceFile)
+        {
+            bool result = false;
+            object sourcePath = sourceFile;
+            object targetPath = GetFileName(sourceFile);
+            object targetType = Microsoft.Office.Interop.Word.WdExportFormat.wdExportFormatPDF; //Word.WdExportFormat.wdExportFormatPDF;
+
+            object paramMissing = Type.Missing;
+            Word.ApplicationClass wordApplication = null;
+            Word.Document wordDocument = null;
+            try
+            {
+                wordApplication = new Word.ApplicationClass();
+                wordDocument = wordApplication.Documents.Open(ref sourcePath);
+
+                if (wordDocument != null)
+                {
+                    wordDocument.SaveAs(targetPath, targetType);
+                    result = true;
+                }
+            }
+            catch
+            {
+                result = false;
+            }
+            finally
+            {
+                if (wordDocument != null)
+                {
+                    wordDocument.Close(ref paramMissing, ref paramMissing, ref paramMissing);
+                    wordDocument = null;
+                }
+                if (wordApplication != null)
+                {
+                    wordApplication.Quit(ref paramMissing, ref paramMissing, ref paramMissing);
+                    wordApplication = null;
+                }
+
+                //ms solution is like this
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+            }
+
+            return result;
+        }
+
+
+
+        string GetFileName(string pathString)
+        {
+            return Path.ChangeExtension(pathString, @".pdf");
+        }
+    }
+}
